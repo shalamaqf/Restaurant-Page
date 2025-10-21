3db6bd7d1cc53fcf686b09aaccae4af3bcce3c50
Shalama <shalamafadillaa@gmail.com>
2025-10-21 14:21:02 +0700

    Create a function to render menu header

--- src/menu.js
+++ src/menu.js
@@ -2,3 +2,18 @@
 import ramenPict from "./ramen.jpg";
 import sushiPict from "./sushi.jpg";
 import curryPict from "./curry.jpg";
+
+// Create a function to render DOM elements for menu as header
+function renderMenuHeader() {
+    const menuHeaderContainer = document.createElement("div");
+    const menuHeader = document.createElement("h1");
+
+    menuHeaderContainer.className = "container menuHeader";
+    menuHeader.id = "menu"
+
+    menuHeader.textContent = "Menu";
+
+    menuHeaderContainer.appendChild(menuHeader);
+
+    return menuHeaderContainer;
+}
