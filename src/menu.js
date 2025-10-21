8b0f35bba24fa124b65a532bafbdcb5e120f2dc4
Shalama <shalamafadillaa@gmail.com>
2025-10-21 14:48:53 +0700

    Create a function to render the ramen menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,5 @@
 // Import food images
-import ramenPict from "./ramen.jpg";
+import ramen from "./ramen.jpg";
 import sushiPict from "./sushi.jpg";
 import curryPict from "./curry.jpg";
 
@@ -17,3 +17,35 @@ function renderMenuHeader() {
 
     return menuHeaderContainer;
 }
+
+// Create a function to render DOM elements for menu ramen
+export function renderRamenMenu() {
+    const ramenMenuContainer = document.createElement("div");
+    const ramenInfoContainer = document.createElement("div");
+    const ramenInfo = document.createElement("p");
+    const bonusDrink = document.createElement("p");
+    const ramenPictContainer = document.createElement("div");
+    const ramenPict = document.createElement("img");
+
+    ramenMenuContainer.className = "containerMenu";
+    ramenInfoContainer.className = "container ramen";
+    ramenInfo.className = "foodInfo ramen";
+    bonusDrink.className = "bonusDrink";
+    ramenPictContainer.className = "pict container";
+    ramenPict.className = "pict ramen";
+    ramenPict.src = ramen;
+
+    ramenInfo.textContent = "Classic Ramen  . . . . . . . . . .  ¥500";
+    bonusDrink.textContent = "+ Signature homemade green tea — on the house.";
+
+    ramenInfoContainer.appendChild(ramenInfo);
+    ramenInfoContainer.appendChild(bonusDrink);
+
+    ramenPictContainer.appendChild(ramenPict);
+
+    ramenMenuContainer.appendChild(ramenInfoContainer);
+    ramenMenuContainer.appendChild(ramenPictContainer);
+
+    return ramenMenuContainer;
+}
+
