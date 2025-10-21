552a2708198f42441b61483b2f0a32d809135976
Shalama <shalamafadillaa@gmail.com>
2025-10-21 13:07:18 +0700

    Create a function to render resto's description

--- src/home.js
+++ src/home.js
@@ -13,6 +13,24 @@ function renderTitle() {
     return titleContainer;
 }
 
+// Create a function to create DOM elements for restaurant's description
+function renderAbout () {
+    const aboutContainer = document.createElement("div");
+    const aboutHeader = document.createElement("h3");
+    const description = document.createElement("p");
+
+    aboutContainer.className = "container about";
+    aboutHeader.className = "header about";
+    description.className = "description";
+
+    aboutHeader.textContent = "About"
+    description.textContent = "A restaurant that offers home-style cooking in a warm, relaxed setting — creating the comforting feeling of coming home."
+
+    aboutContainer.appendChild(aboutHeader);
+    aboutContainer.appendChild(description);
+
+    return aboutContainer;
+}
 
 
 
