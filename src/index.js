1b3a3db3514ade8831bf3683afe51362959e252e
Shalama <shalamafadillaa@gmail.com>
2025-10-22 15:09:34 +0700

    Create a function to append a page to content

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 import { renderHomePage } from "./home";
 import { renderMenuPage } from "./menu";
 import { renderContactPage } from "./contact";
+
+// Create a function to append a page to content container
+function appendPage(page) {
+    const contentPage = document.querySelector("#content");
+
+    contentPage.appendChild(page);
+}
