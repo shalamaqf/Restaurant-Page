a6e4519ec9f7d11cadf85de92a7ded003cb89b76
Shalama <shalamafadillaa@gmail.com>
2025-10-22 13:43:46 +0700

    Import modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("test");
+// Import modules 
+import { renderHomePage as homePage } from "./home";
+import { renderMenuPage as menuPage } from "./menu";
+import { renderContactPage as contactPage } from "./contact";
