73cce76238055ef042998c5ce9275edf944da7ae
Shalama <shalamafadillaa@gmail.com>
2025-10-22 14:46:41 +0700

    Create a function to append home page to content

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@
 import { renderHomePage as homePage } from "./home";
 import { renderMenuPage as menuPage } from "./menu";
 import { renderContactPage as contactPage } from "./contact";
+
+// Create a function to appen home page to content containers
+function appendHomePage() {
+    const contentPage = document.querySelector("#content");
+
+    contentPage.appendChild(homePage());
+}
