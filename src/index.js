1e043291fefd821a244213ef4498f7a8365dc0df
Shalama <shalamafadillaa@gmail.com>
2025-10-22 15:10:15 +0700

    Create a function to prevent duplicate content

--- src/index.js
+++ src/index.js
@@ -9,3 +9,14 @@ function appendPage(page) {
 
     contentPage.appendChild(page);
 }
+
+// Create a function to prevent content duplicate
+function isExist(page) {
+    const contentPage = document.querySelector("#content");
+
+    if (contentPage.contains(page)){
+        return true;
+    }
+
+    return false;
+}
