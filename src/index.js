58c1a57ba3869e7dde8d39978e2e42c66aa1c3b0
Shalama <shalamafadillaa@gmail.com>
2025-10-22 14:48:21 +0700

    Create a function to append menu page to content

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import { renderHomePage as homePage } from "./home";
 import { renderMenuPage as menuPage } from "./menu";
 import { renderContactPage as contactPage } from "./contact";
 
-// Create a function to appen home page to content containers
+// Create a function to append home page to content containers
 function appendHomePage() {
     const contentPage = document.querySelector("#content");
 
     contentPage.appendChild(homePage());
+}
+
+
+// Create a function to append menu page to content containers
+function appendMenuPage() {
+    const contentPage = document.querySelector("#content");
+
+    contentPage.appendChild(menuPage());
 }
