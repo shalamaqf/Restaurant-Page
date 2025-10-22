ce3e9fb5c2cf20b6ef2003d1f8320fd607968d66
Shalama <shalamafadillaa@gmail.com>
2025-10-22 15:08:34 +0700

    Delete alias and functions to append page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
 // Import modules 
-import { renderHomePage as homePage } from "./home";
-import { renderMenuPage as menuPage } from "./menu";
-import { renderContactPage as contactPage } from "./contact";
-
-// Create a function to append home page to content containers
-function appendHomePage() {
-    const contentPage = document.querySelector("#content");
-
-    contentPage.appendChild(homePage());
-}
-
-
-// Create a function to append menu page to content containers
-function appendMenuPage() {
-    const contentPage = document.querySelector("#content");
-
-    contentPage.appendChild(menuPage());
-}
-
-// Create a function to append contact page to content containers
-function appendContactPage() {
-    const contentPage = document.querySelector("#content");
-
-    contentPage.appendChild(contactPage());
-}
+import { renderHomePage } from "./home";
+import { renderMenuPage } from "./menu";
+import { renderContactPage } from "./contact";
